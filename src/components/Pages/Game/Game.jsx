ac01d2515d7e40be4b2ece60a3f8037f8c84120c
unknown <ratiry@yandex.ru>
2023-07-21 17:05:14 +0300

    used random function to pick first player to move

--- src/components/Pages/Game/Game.jsx
+++ src/components/Pages/Game/Game.jsx
@@ -5,24 +5,26 @@ import { useEffect ,useState} from "react";
 import GenerateStack from "../../Helpers/GenerateStackFunction";
 import CardsData from "../../Helpers/Cards";
 import Dock from "./Dock/Dock";
+import getRandomInt from "../../Helpers/Random";
 let Game=()=>{
     let location=useLocation();
     let quantityOfPlayers=Number(location.state.ammountOfPlayers);
     let [stackOfCards,setStackOfCards]=useState([]);
     let [cardsOfPlayers,setCardsOfPlayers] = useState([]);
-
+    let [numberOfCurrentPlayer,SetnumberOfCurrentPlayer] = useState(0);
     useEffect(()=>{    
        let [remainingStack,firstPlayersCards] =GenerateStack(CardsData,quantityOfPlayers);
        setCardsOfPlayers(firstPlayersCards);
-       setStackOfCards(remainingStack)
+       setStackOfCards(remainingStack);
+       SetnumberOfCurrentPlayer(getRandomInt(quantityOfPlayers));
     },[])
 
     return(
         <div className={classes.Game}> 
             <Dock cards={stackOfCards}/>
-            <Player cards={cardsOfPlayers[2]} id={2} quantityOfPlayers={quantityOfPlayers}/>
+            <Player cards={cardsOfPlayers[1]} id={2} quantityOfPlayers={quantityOfPlayers}/>
             <div></div>
-            <Player cards={cardsOfPlayers[1]} id={3} quantityOfPlayers={quantityOfPlayers}/>
+            <Player cards={cardsOfPlayers[2]} id={3} quantityOfPlayers={quantityOfPlayers}/>
             <div className={classes.PlayField}></div>
             <Player cards={cardsOfPlayers[3]} id={4} quantityOfPlayers={quantityOfPlayers}/>
             <div></div>
