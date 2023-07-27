3068219cce7e3f4122197854d5bdfa518ef4f37d
unknown <ratiry@yandex.ru>
2023-07-27 13:01:02 +0300

    edited cards scss so cards dont have negative margin

--- src/components/Pages/Game/Dock/Dock.jsx
+++ src/components/Pages/Game/Dock/Dock.jsx
@@ -1,11 +1,14 @@
 import Card from "../Card/Card";
 import classes from "./Dock.module.scss";
+import P from "../../../Common/Typography/P/P";
 let Dock=(props)=>{
     
     return(
         <div className={classes.dock}>
             {props.cards.map((card)=><Card position={"dock"}/>)}
-
+            <div className={classes.ammountOfDockCards}>
+            <P>{props.cards.length} left</P>
+            </div> 
         </div>
     )
 }
