98362f6ba9004e93a88251f6500a498e62049d17
unknown <ratiry@yandex.ru>
2023-07-27 13:43:53 +0300

    changed dock card

--- src/components/Pages/Game/Dock/Dock.jsx
+++ src/components/Pages/Game/Dock/Dock.jsx
@@ -5,7 +5,8 @@ let Dock=(props)=>{
     
     return(
         <div className={classes.dock}>
-            {props.cards.map((card)=><Card position={"dock"}/>)}
+            {/* {props.cards.map((card)=><Card position={"dock"}/>)} */}
+            {props.cards.length>0 && <Card position={"dock"}/>}
             <div className={classes.ammountOfDockCards}>
             <P>{props.cards.length} left</P>
             </div> 
